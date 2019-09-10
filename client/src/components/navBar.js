72eac1a4d1ca46f00678f4871343c713cc5a6afd
Mirwais Nezami <mirwais79@hotmail.com>
2019-09-10 15:00:15 +0200

    navbar with logo and button copl

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,46 +1,36 @@
-import React from "react";
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Navbar, Nav, Button, Row, Col } from 'react-bootstrap';
 import Logo from '../images/logo.png';
 import LogInUser from '../images/user.png';
 
 
+class NavBar extends Component {
 
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar bg="light" expand="lg">
+          <Row className="logo">
+            <Col xs={6} md={4}>
+              <img src={Logo} height="55" width="55" alt="Nav-Logo" />
+            </Col>
+          </Row>
+          <Navbar.Brand className='logoHeader' href="/">Recept & Näring</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/"></Nav.Link>
+              <div className="navButtons">
+                <Button className="recipeButton" variant="outline-secondary" href="/recipe">Lägg Nytt Recept</Button>
+                <Button className="logInButton" variant="outline-secondary" href="/logIn"> <img src={LogInUser} height="30" width="30" alt="Nav-Logo" /> Logga in</Button>
+              </div>
+            </Nav>
 
-const NavBar = () => {
-
-  const navLogo = (
-    <span className="logo">
-      <a href="/">
-        <img src={Logo} height="40" width="40" alt="Nav-Logo" /></a>
-    </span>
-  );
-
-  const logoInUser = (
-    <span className="userInButton">
-      <a href="/logIn">
-        <img src={LogInUser} height="20" width="20" alt="Person logo in button" /></a>
-    </span>
-  );
-
-  return (
-    <React.Fragment>
-      <Navbar bg="light" expand="lg">
-        {navLogo}
-        <Navbar.Brand className='logoHeader' href="/">Recept & Näring</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/"></Nav.Link>
-            <div className="navButton">
-              <Button variant="outline-secondary" href="/recipe">Lägg Nytt Recept</Button>
-              <Button variant="outline-secondary" href="/logIn">{logoInUser}Logga in</Button>
-            </div>
-          </Nav>
-
-        </Navbar.Collapse>
-      </Navbar>
-    </React.Fragment>
-  );
+          </Navbar.Collapse>
+        </Navbar>
+      </React.Fragment>
+    );
+  };
 };
 
 export default NavBar;
