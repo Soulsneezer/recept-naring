636ccbfe8ef4b238557f59520710f9043741a958
Robin <r.asplund@mail.com>
2019-09-10 14:59:01 +0200

    footer done i think

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -8,29 +8,34 @@ class Footer extends React.Component{
 
     render(){
         return(
-            <div className="container-fluid bg-light">
-                <div className="row">
-                    <div className="col-4">
+            <footer className="container-fluid bg-light">
+
+                <div className="row ">
+
+                    <div className="col-8 col-sm-4 p-2">
                         <div className="footer-content-box" >
                             1355 Market St, Suite 900 <br/>
                             San Fransisco, CA 94103
                         </div>                     
                     </div>
-                    <div className="col-4 order-3 order-sm-2">
+
+                    <div className="col-4 mt-3">
                         <div className="footer-content-box icons" >
-                            <img src={facebook} className="icon"></img>
-                            <img src={twitter} className="icon"></img>
-                            <img src={instagram} className="icon"></img>
+                            <a href="http://facebook.com"><img src={facebook} className="icon p-1"></img></a>
+                            <a href="http://twitter.com"><img src={twitter} className="icon p-1"></img></a>
+                            <a href="http://instagram.com"><img src={instagram} className="icon p-1"></img></a>
                         </div>
                     </div>
-                    <div className="col-4 order-sm-2">
+
+                    <div className="col-8 col-sm-4 p-2">
                         <div className="footer-content-box" >
                             +15 (123) 456-7890 <br/>
                             grupp.3@gmail.com
                         </div>
                     </div>
+
                 </div>
-            </div>
+            </footer>
         )
     }
 
