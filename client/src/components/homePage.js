e960173fd2345a996c8b82456165a2b9ea2609ed
sunsneezer <xil_wong@hotmail.com>
2019-09-14 13:22:50 +0200

    added search functions

--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
-import {InputGroup, FormControl, Button, Card, Img, Body, Title, Text} from 'react-bootstrap';
+import {InputGroup, FormControl, Button, Container, Row, Col, Card, Title} from 'react-bootstrap';
 import REST from "../REST.js";
 import { Link } from "react-router-dom";
 
-class Recipe extends REST {
-  static get baseRoute() {
-    return "recipes";
-  }
-}
+
+class Recipe extends REST {}
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
 
     this.searchHandler = this.searchHandler.bind(this);
+    this.showMoreRecipes = this.showMoreRecipes.bind(this);
 
     this.state = {
       recipes: [],
-      searchInput:''
+      searchInput:'',
+      count: 4,
+      recipesLength: 0
     }
   }
 
@@ -28,16 +28,30 @@ class HomePage extends Component {
     })
 
     let recipes = await Recipe.find('/' + searchInput);
+    let recipesLength = recipes.length;
+    recipes = recipes.slice(0, this.state.count);
     if(searchInput){
       this.setState({
-        recipes: recipes
+        recipes: recipes,
+        recipesLength: recipesLength
       });
     } else {
       this.setState({
-        recipes: []
+        recipes: [],
+        count: 4
       });
     }
-    
+  }
+
+  async showMoreRecipes() {
+    this.state.count += 4;
+    let recipes = await Recipe.find('/' + this.state.searchInput);
+    recipes = recipes.splice(0,this.state.count);
+
+    this.setState({
+      recipes: recipes,
+      count: this.state.count
+    })
   }
 
   render() {
@@ -54,21 +68,35 @@ class HomePage extends Component {
               aria-describedby="Sök efter recept här..."
             />
           </InputGroup>
-        </div>        
+        </div>
 
-        {this.state.recipes.map(recipe => (
-          <Card key={recipe._id} style={{ width: '18rem' }}>            
-            <Card.Img variant="top" src={require("../images/"+ recipe.img)} alt={recipe.img} />
-            <Card.Body>
-              <Card.Title>{recipe.name}</Card.Title>
-              <Card.Text>
-                {recipe.startText}
-              </Card.Text>
-              <Button href={"/recipe/" + recipe._id} variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        ))}
-
+        {this.state.recipes.length === 0 ? '': 
+          <Container className="container-outer">
+            <Row className="container-title">
+              <Col>
+                <h1 className="card-h1">Sökresultat</h1>
+              </Col>
+            </Row>
+            <Row className="container-inner">
+              {this.state.recipes.map(recipe => (
+                <div className="card-outer special" key={recipe._id}> 
+                  <div className="card-inner">
+                    <img className="card-img" src={require("../images/"+ recipe.img)} alt={"En bild på " + recipe.name} />
+                    <Card.Title className="card-title">
+                      <h5 className="card-p">{recipe.name}</h5>
+                    </Card.Title>
+                  <Button className="card-button" href={"/recipe/" + recipe._id}>Gå till recept</Button>
+                  </div>           
+                </div>
+              ))}
+            </Row>
+            <Button 
+              className="show-more-btn" 
+              onClick={this.showMoreRecipes}>
+              {this.state.recipesLength <= this.state.count ? 'Inga fler recept': 'Visa fler recept'}
+            </Button>
+          </Container>
+        }
       </div>
     );
   }
