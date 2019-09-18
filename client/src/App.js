d06b6103a70fd025a9bdd3c17663e6b3de64b4a0
Mirwais Nezami <42848648+Miro7979@users.noreply.github.com>
2019-09-18 14:19:58 +0200

    Revert "Merge pull request #16 from Soulsneezer/Dev-ReactBoot"
    This reverts commit f951d73741b06839d7f1550d1da8ae6729fcaf68.

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,24 @@ import NavBar from "./components/navBar";
 import NotFound from "./components/notFound";
 import HomePage from './components/homePage';
 import Recipe from './components/recipe';
-import ReadRecipe from './components/readRecipe'
 import LogIn from './components/logIn';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { read } from "fs";
+
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className='App'>
+        <div className="App">
           <NavBar />
           <Switch>
             <Route path="/" exact component={HomePage} />
             <Route path="/recipe" component={Recipe} />
             <Route path="/logIn" component={LogIn} />
-            <Route path="/read-recipe/:id" component={ReadRecipe} />
             <Route path="/not-found" component={NotFound} />
           </Switch>
           <Footer />
