8f781ec39323806023b0555c7e9b7b7e7fc4a556
Mirwais Nezami <42848648+Miro7979@users.noreply.github.com>
2019-09-18 14:42:23 +0200

    Revert "Dev react boot"

--- client/src/components/foodCardContainer.js
+++ client/src/components/foodCardContainer.js
@@ -1,41 +1,28 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import FoodCard from './foodCard.js'
-import { Col, Row, Container } from 'react-bootstrap';
-import REST from "../REST"
+import { Col, Row } from 'react-bootstrap';
 
-class Recipe extends REST{}
 export default class FoodCardContainer extends Component {
   constructor(props) {
     super(props);
     Object.assign(this, props);
-    this.loadShows()
-    this.recipesFound = []
+    this.state = {};
     // this.handleClick = this.handleClick.bind(this);
   }
-  async loadShows(){
-    this.recipesFound = await Recipe.find(`.find()`);
-    this.setState({ state: this.state });
 
-    console.log(this.recipesFound)
-  }
   render() {
     return (
       <React.Fragment>
-      <Container className="container-outer">
         <div className="foodCardContainer">
           <Row>
             <Col sm={8} md={8} lg={8}>
               <div className="innerFoodCardContainer">
-              {this.recipesFound.map( recipe => {
-                return <FoodCard key={recipe._id} {...recipe}/>
-              }
-              )}
+                <FoodCard />
               </div>
             </Col>
           </Row>
         </div>
-      </Container>
       </React.Fragment>
     );
   }
