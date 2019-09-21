559456f72f6aea3a08f807917df6cf3ef276ec5d
melli <melsie78@hotmail.com>
2019-09-21 11:52:24 +0200

    made addUnits better :)
    one can choose the unit and it changes in the buttons "face"

--- client/src/components/AddRecipeUnits.js
+++ client/src/components/AddRecipeUnits.js
@@ -8,7 +8,7 @@ class AddRecipeUnits extends React.Component {
         super(props);
 
         this.state = {
-            mySelection: 'id',
+            mySelection: 'måttet',
         };
     }
 
@@ -19,6 +19,8 @@ class AddRecipeUnits extends React.Component {
         });
     }
     render() {
+        
+
         return (
             <React.Fragment>
                 <Dropdown
@@ -28,71 +30,82 @@ class AddRecipeUnits extends React.Component {
                     <Dropdown.Toggle
                         className="dropdown-units pl-2"
                         variant="outline-secondary">
-                        mått
+                        {this.state.mySelection}
                     </Dropdown.Toggle>
                     <Dropdown.Menu className="units-dropdown-menu">
                     
                         <Dropdown.Item
                             className="dropdown-link"
+                            href="#/action-1"
                             eventKey="1"
                             id='st'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[0]}</Dropdown.Item>
 
 
                         <Dropdown.Item
                             className="dropdown-link"
-
+                            href="#/action-2"
                             eventKey="2"
                             id='ml'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[1]}</Dropdown.Item>
 
                         <Dropdown.Item
                             className="dropdown-link"
-
+                            href="#/action-3"
                             eventKey="3"
                             id='dl'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[2]}</Dropdown.Item>
                         <Dropdown.Item
                             className="dropdown-link"
                             href="#/action-4"
                             eventKey="4"
                             id='l'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[3]}</Dropdown.Item>
                         <Dropdown.Item
                             className="dropdown-link"
                             href="#/action-5"
                             eventKey="5"
                             id='mg'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[4]}</Dropdown.Item>
                         <Dropdown.Item
                             className="dropdown-link"
                             href="#/action-6"
                             eventKey="6"
                             id='g'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[5]}</Dropdown.Item>
                         <Dropdown.Item
                             className="dropdown-link"
                             href="#/action-7"
                             eventKey="7"
                             id='kg'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[6]}</Dropdown.Item>
                         <Dropdown.Item
                             className="dropdown-link"
                             href="#/action-8"
                             eventKey="8"
                             id='krm'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[7]}</Dropdown.Item>
                         <Dropdown.Item
                             className="dropdown-link"
                             href="#/action-9"
                             eventKey="9"
                             id='tsk'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[8]}</Dropdown.Item>
                         <Dropdown.Item
                             className="dropdown-link"
                             href="#/action-10"
                             eventKey="10"
                             id='msk'
+                            value={this.mySelection}
                             onClick={e => this.selectedDropdown(e)}>{units[9]}</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
