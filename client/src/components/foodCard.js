eeb274385783364f4d03366cb1e590c4fb1d438e
Mirwais Nezami <mirwais79@hotmail.com>
2019-09-24 15:46:19 +0200

    Go to recipe button added

--- client/src/components/foodCard.js
+++ client/src/components/foodCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
+import { CardFooter } from "react-bootstrap/Card";
 
 
 export default class FoodCard extends Component {
@@ -17,13 +18,22 @@ export default class FoodCard extends Component {
     return (
       <div className="show-recipe">
         <Card sm={4} md={7} lg={6} xl={6} style={{ width: 'auto' }}>
-          <Card.Img variant="top" src={require('../images/' + this.state.imgUrl)} alt={'En bild på ' + this.props.recipe.name}/>
+          <Card.Img variant="top" src={require('../images/' + this.state.imgUrl)} alt={'En bild på ' + this.props.recipe.name} />
           <Card.Body>
             <Card.Title className="card-title-h5">{this.props.recipe.name}</Card.Title>
             <Card.Text>
-              {this.props.recipe.startText.slice(0,30) + '...'}
+              {this.props.recipe.startText.slice(0, 30) + '...'}
             </Card.Text>
           </Card.Body>
+          <Card.Footer>
+            <Button
+              className='card-button'
+              href={'/recipe/' + this.props.recipe._id}
+              aria-label="knapp för att gå till recept"
+            >
+              Se receptet
+            </Button>
+          </Card.Footer>
         </Card>
       </div>
     );
