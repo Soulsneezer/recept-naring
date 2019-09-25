1a92f06d4bb70adc53f577275b0eccc9325b19b6
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2019-09-25 11:36:51 +0200

    Wip beginning of dropdown

--- client/src/components/AddRecipeIngredientRow.js
+++ client/src/components/AddRecipeIngredientRow.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import { Form, Col, Row } from 'react-bootstrap';
+import { Form, Col, Row, Dropdown, Menu } from 'react-bootstrap';
 import RemoveRowButton from './AddRecipeRemoveButton';
 import AddRecipeUnits from './AddRecipeUnits';
-
+import REST from '../REST.js';
+import { Typeahead } from 'react-bootstrap-typeahead';
 const uuid4 = require('uuid/v4');
+class Makronutrient extends REST{}
+//{makronutrient: Makronutrient, index: Makronutrient.name.indexOf(searchterm)}
+
 
 class AddRecipeIngredientRow extends Component {
+  constructor(props){
+    super(props);
+    this.searchHandler = this.searchHandler.bind(this);
 
+    this.state = {
+      searchInput: "",
+      results: [],
+      selected: []
+    }
+  }
+    
+    async searchHandler(e){
+      let searchInput = e.target.value;
+      searchInput = searchInput.trim();
+      this.setState({
+        searchInput: e.target.value
+      });
+      if(searchInput.length > 3){
+        console.log("hej")
+        let foundIngredient =  await Makronutrient.find(
+          `.find({name:/${searchInput}/i})`);
+          this.setState({
+            results: foundIngredient
+          })
+        let alla = await Makronutrient.find()
+        console.log(this.state.results)
+        console.log(foundIngredient)
+      }
+    }
   render() {
     const id = uuid4();
 
@@ -20,7 +52,17 @@ class AddRecipeIngredientRow extends Component {
             <AddRecipeUnits />
           </Col>
           <Col xs={10} sm={11} md={5} lg={6} className="mt-2">
-            <Form.Control className="recipe-name" input="true" placeholder="ingrediensen" aria-labelledby="ingrediensen" />
+            <Form.Control className="recipe-name" input="true" placeholder="ingrediensen" aria-labelledby="ingrediensen"onChange={this.searchHandler} value={this.state.searchInput} />
+              {this.state.results ?  <Typeahead
+                    {...this.state}
+                    id="basic-example"
+                    onChange={selected => this.setState({ selected })}
+                    options={this.state.results.name}
+                    placeholder="Choose a state..."
+                    labelKey="name"
+                  />
+                : null
+                }
           </Col>
           <Col xs={1} md={1} className="MdARemoveCircleOutline align-right mt-2">
             <RemoveRowButton key={id} onClick={() => this.props.deleteMe(this.props.remove)} aria-label="radera en ingrediensrad" />
